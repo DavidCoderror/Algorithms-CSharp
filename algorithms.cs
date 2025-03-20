acbde7d0db4dc8da8d11a52887b1d9854537122d
Beatle <123680498+DavidCoderror@users.noreply.github.com>
2025-03-20 12:06:26 -0400

    Create algorithms.cs

---
+++ algorithms.cs
@@ -0,0 +1,43 @@
+void triInsertion(int[] tab)
+{
+    int n = tab.Length;
+    for (int i = 1; i <= n -1; i++){
+        int elementCle = tab[i];
+        int j = i;
+
+        while (j > 0 && tab[j - 1] > elementCle)
+        {
+            tab[j] = tab[j - 1];
+            j--;
+        }
+
+        tab[j] = elementCle;
+    }
+
+}
+
+void triShell(int[] tab)
+{
+    int n = tab.Length;
+    int[] intervals = new int[] { 1750,710,301,132,57,23,10,4,1};
+
+    foreach(int interval in intervals)
+    {
+        triInsertionInterval(tab, intervals[interval]);
+    }
+}
+
+void triInsertionInterval(int[] tab, int interval){
+    int n = tab.Length;
+    for (int i = 1; interval <= n - 1; i++)
+    {
+        int elementCle = tab[i];
+        int j = i - interval;
+        while (j>=0 && tab[j] > elementCle)
+        {
+            tab[j + interval] = tab[j];
+            j -= interval;
+        }
+        tab[j +interval] = elementCle;
+    }
+}
